6bfce130a0ca4b4df50ec9cdc008d88adc001de1
RasmusOlesk <rasmus.olesk12@gmail.com>
2026-01-20 21:54:01 +0200

    yo

--- Kodutöö/Keerukamad_arvutused.py
+++ Kodutöö/Keerukamad_arvutused.py
@@ -1,29 +1,34 @@
 """Math exercises vol2."""
 
+import math
+
 
 def time_converter(seconds: int) -> str:
     """Convert time in seconds to hours and minutes."""
-    # Write your code here
-
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
     return f"{seconds} sekundit on {hours} tund(i) ja {minutes} minut(it)."
 
 
 def student_helper(angle: int) -> str:
     """Return the sine and cosine of the given angle in degrees."""
-    # Write your code here
+    radians = math.radians(angle)
+    sine = math.sin(radians)
+    cosine = math.cos(radians)
     return f"Nurk: {angle}, siinus: {sine}, koosinus: {cosine}."
 
 
 def greetings(n: int) -> str:
-    """Return a string that contains "Hey" n times."""
-    # Write your code here
+    """Return a string that contains 'Hey' n times."""
+    lots_of_heys = "Hey" * n
     return lots_of_heys
 
 
 def adding_numbers(num_a: int, num_b: int) -> str:
     """Return given numbers added together as a string."""
-    # Write your code here
+    string_numbers = str(num_a + num_b)
     return string_numbers
 
 
 if __name__ == '__main__':
+    pass

63dac04c74a1a21e632261c60b11131bacbc2c55
opilane <@RasmusOlesk>
2026-03-10 11:46:52 +0200

    Kontrolltöö 2

---
+++ Kontrolltöö_2/KT2_1.py
@@ -0,0 +1,19 @@
+def ask_name_and_phone_number():
+    """Küsib inimeste nimesi ja telefoni numbreid"""
+    name_1 = print(input("Mis on esimese inimese nimi? : "))
+    name_2 = print(input("Mis on teise inimese nimi? : "))
+    name_3 = print(input("Mis on komanda inimese nimi? : "))
+    phone_number_1 = print(input("Mis on esimese inimese telefoni number? : "))
+    phone_number_2 = print(input("Mis on teise inimese telefoni number? : "))
+    phone_number_3 = print(input("Mis on kolmanda inimese number telefoni number? : "))
+
+def ask_for_name_or_number():
+    """Küsib nime järgi numbrit ja numbri järgi nime"""
+
+
+
+if __name__ == '__main__':
+    ask_name_and_phone_number()
+    ask_for_name_or_number()
+
+

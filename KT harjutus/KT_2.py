0310d541cba98e026e33564297f598781614a493
RasmusOlesk <rasmus.olesk12@gmail.com>
2026-03-10 19:09:27 +0200

    lamp update

--- KT harjutus/KT_2.py
+++ KT harjutus/KT_2.py
@@ -1,16 +1,21 @@
-nimi = input("Sisesta oma nimi: ")
-vanus = int(input("Sisesta oma vanus: "))
+def pea_programm():
+    nimi = input("Sisesta oma nimi: ")
+    vanus = int(input("Sisesta oma vanus: "))
 
-if len(nimi) < vanus or vanus < 5:
-    for _ in range(3):
-        print(f"Tere, {nimi}!")
+    # Tervita ainult, kui nimi on lühem kui vanus ja vanus < 5
+    if len(nimi) < vanus and vanus < 5:
+        for _ in range(3):
+            print(f"Tere, {nimi}!")
     else:
-        arv1 = int(input("Sisesta esimene arv: "))
-        arv2 = int(input("Sisesta teine arv: "))
-        arv3 = int(input("Sisesta kolmas arv: "))
+        # Küsi kolm arvu ja kontrolli sum
+        a = int(input("Sisesta esimene arv: "))
+        b = int(input("Sisesta teine arv: "))
+        c = int(input("Sisesta kolmas arv: "))
         summa = int(input("Mis on nende arvude summa? "))
-
-        if arv1 + arv2 + arv3 == summa:
+        if a + b + c == summa:
             print("Õige tulemus!")
         else:
             print("Vale tulemus!")
+
+if __name__ == '__main__':
+    pea_programm()

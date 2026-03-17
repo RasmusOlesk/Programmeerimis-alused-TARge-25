52fa68ffcfe8803035c2968c5c1e5a7b2df5d368
Rasmus Olesk <@RasmusOlesk>
2026-03-17 11:44:40 +0200

    17.03.2026 testimine ja rekursioon tunni ja kodutöö

---
+++ Kodutöö/Testimine iseseisev töö/test.py
@@ -0,0 +1,9 @@
+"""Tests for solution."""
+import pytest
+
+from solution import students_study, lottery, fruit_order
+
+
+def test__students_study__night_with_coffee__no_studying():
+    """During night with coffee students do not study."""
+    assert students_study(3, True) is False
